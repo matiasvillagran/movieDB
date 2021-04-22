0e984b661ae23d35187d2b15b81a9bbacf1fd6ba
Matias Villagran <matias.villagran01@gmail.com>
2021-04-22 15:08:14 -0400

    JIRACODE model was added

---
+++ movieDB/modules/moviesList/Model/MovieData.swift
@@ -0,0 +1,23 @@
+//
+//  MovieData.swift
+//  movieDB
+//
+//  Created by Matias Villagran on 22-04-21.
+//
+
+import Foundation
+
+struct MovieData : Codable, Hashable {
+    var page: Int
+    let results: [Movie]
+    let total_pages, total_results: Int
+}
+
+struct Movie : Codable, Identifiable, Hashable {
+    var id: Int
+    let adult: Bool
+    let original_title: String
+    let poster_path: String
+    let overview: String
+    let vote_average: Double
+}
