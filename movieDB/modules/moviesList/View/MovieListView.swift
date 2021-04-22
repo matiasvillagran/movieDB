4c23964fdaa2071212cfa4920cfd3284fbf11f66
Matias Villagran <matias.villagran01@gmail.com>
2021-04-22 17:37:12 -0400

    JIRACODE added list with movies fetched

--- movieDB/modules/moviesList/View/MovieListView.swift
+++ movieDB/modules/moviesList/View/MovieListView.swift
@@ -6,12 +6,42 @@
 //
 
 import SwiftUI
+import Kingfisher
 
 struct MovieListView: View {
+    
+    @ObservedObject var model = MovieListViewModel(data: ApiManager())
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+            NavigationView{
+                TabView{
+                    List(model.popularMovies) { movie in
+        
+                                VStack {
+                                    ZStack {
+                                        KFImage.url(URL(string: "https://image.tmdb.org/t/p/w500\(movie.poster_path)")!).resizable()
+                                            .loadDiskFileSynchronously()
+                                            .cacheMemoryOnly()
+                                            .fade(duration: 0.25)
+                                            .aspectRatio(16/15, contentMode: .fill)
+                                            .cornerRadius(8)
+                                    }
+                                    Text(movie.original_title)
+                                        .font(.headline).bold()
+                                    Spacer()
+                                }
+                    }
+                        .tabItem {
+                            Image(systemName: "appletv.fill")
+                            Text("Most Popular")
+                            
+                        }                    
+                }
+                .animation(.easeIn(duration: 0.1))
+                .navigationBarTitle("Movies", displayMode: .inline)
+            }
+            
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
